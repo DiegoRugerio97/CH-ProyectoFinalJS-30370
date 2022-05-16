49093650cd6bed0da280c5d7732c273ed9f11c6d
DiegoRugerio97 <diego.rugerioc97@gmail.com>
2022-05-16 12:37:53 -0500

    Primera implementacion de ISR

---
+++ JS/ISR.js
@@ -0,0 +1,98 @@
+// ***********************************************************************************************************************
+// ISR
+// Constantes para calculo de retencion de impuestos
+// Clase limite
+class limite {
+    constructor(limiteInferior, limiteSuperior, cuotaFija, porcentaje) {
+        this.limiteInferior = limiteInferior;
+        this.limiteSuperior = limiteSuperior;
+        this.cuotaFija = cuotaFija;
+        this.porcentaje = porcentaje;
+        this.retencion;
+    }
+    calcularRetencion(salarioMensual) {
+        this.retencion = Math.round(((salarioMensual - this.limiteInferior) * this.porcentaje) + this.cuotaFija);
+    }
+}
+// Array de objetos limite
+const LIMITES_ISR = [new limite(0, 644.58, 0, .0192),
+new limite(644.58, 5470.92, 12.38, 0.064),
+new limite(5470.92, 9614.66, 321.26, 0.1088),
+new limite(9614.66, 11176.62, 772.1, 0.16),
+new limite(11176.62, 13381.47, 1022.01, 0.1792),
+new limite(13381.47, 26988.5, 1417.12, 0.2136),
+new limite(26988.5, 42537.58, 4323.58, 0.2352),
+new limite(42537.58, 81211.25, 7980.73, 0.3),
+new limite(81211.25, 108281.67, 19582.83, 0.32),
+new limite(108281.67, 324845.01, 28245.36, 0.34),
+new limite(324845.01, 10000000, 101876.9, 0.35)];
+
+const crearTablaLimites = (arrayLimites) => {
+    let tableBody = document.getElementById("tableBodySegmentos");
+    arrayLimites.forEach((limite) => {
+        let tableRow = document.createElement("tr");
+        let contenidoRow = `<td>$ ${limite.limiteInferior.toLocaleString('en-Latn-US')}</td>
+        <td>$ ${limite.limiteSuperior.toLocaleString('en-Latn-US')}</td>
+        <td>$ ${limite.cuotaFija.toLocaleString('en-Latn-US')}</td>
+        <td>${(limite.porcentaje * 100).toFixed(2)}%</td>`;
+        tableRow.innerHTML = contenidoRow;
+        tableBody.append(tableRow);
+    });
+}
+crearTablaLimites(LIMITES_ISR);
+
+// Input
+const inputSalarioMensual = document.getElementById("inputSalarioISR");
+// Boolean para validacion
+let salarioMensualCorrecto = false;
+// Boton
+const botonISR = document.getElementById("botonISR");
+// Validacion del input
+const validacionSalarioISR = (e) => {
+    if (e.target.value <= 0) {
+        e.target.className = "form-control campoTexto invalido";
+        salarioMensualCorrecto = false;
+        botonAguinaldo.setAttribute("disabled", "");
+        crearAlerta();
+    }
+    else {
+        e.target.className = "form-control campoTexto";
+        salarioMensualCorrecto = true;
+        botonISR.removeAttribute("disabled");
+        limpiarAlerta();
+    }
+}
+// Se agrega el event listener al input
+inputSalarioMensual.addEventListener("blur", validacionSalarioISR);
+
+// Resultados de la retencion
+const resultadosISR = document.getElementById("resultadosISR");
+
+// Logica refactorizada del calculo del ISR
+const calcularISR = (salarioMensual) => {
+    // Calculo de la retencion del ISR sobre el salario, basado en los limites establecidos por la ley federal del trabajo mexicana.
+    // Variable para guardar el objeto limite despues de segmentar el salario
+    let limiteAsignado;
+    // Implementacion de logica de segmentado utilizando un array de objetos limite.
+    limiteAsignado = LIMITES_ISR.find((lim) => { return salarioMensual > lim.limiteInferior && salarioMensual <= lim.limiteSuperior });
+    // Metodo para el calculo de la retencion.
+    limiteAsignado.calcularRetencion(salarioMensual);
+    return limiteAsignado;
+}
+
+// Handler para el submit
+const ISRHandler = (e) => {
+    e.preventDefault();
+    // Variable para guardar el salario mensual del usuario
+    const salarioMensual = valorInput(inputSalarioMensual);
+    const ISR = calcularISR(salarioMensual);
+    resultadosISR.innerHTML = `<div>
+                                    <span>Con un salario mensual de: $${salarioMensual.toLocaleString('en-Latn-US')} (MXN).</span>
+                                    <span>La retención del ISR según la ley federal del trabajo es de: $${ISR.retencion.toLocaleString('en-Latn-US')} (MXN).<span/><br>
+                                    <span>Resultando en un ingreso neto de: $${(salarioMensual - ISR.retencion).toLocaleString('en-Latn-US')} (MXN). <span/><br>
+                                    </div>`;
+    // e.target.reset();
+}
+// Se asigna el event listener
+const formISR = document.getElementById("formISR");
+formISR.addEventListener("submit", ISRHandler);
--- JS/main.js
+++ JS/main.js
@@ -17,33 +17,6 @@
 const DIAS_MES = 30.5;
 const DIAS_ANIO = 365;
 
-// Constantes para calculo de retencion de impuestos
-// Clase limite
-class limite {
-    constructor(limiteInferior, limiteSuperior, cuotaFija, porcentaje) {
-        this.limiteInferior = limiteInferior;
-        this.limiteSuperior = limiteSuperior;
-        this.cuotaFija = cuotaFija;
-        this.porcentaje = porcentaje;
-        this.retencion;
-    }
-    calcularRetencion(salarioMensual) {
-        this.retencion = Math.round(((salarioMensual - this.limiteInferior) * this.porcentaje) + this.cuotaFija);
-    }
-}
-
-// Array de objetos limite
-const LIMITES_ISR = [new limite(0, 644.58, 0, .0192),
-new limite(644.58, 5470.92, 12.38, 0.064),
-new limite(5470.92, 9614.66, 321.26, 0.1088),
-new limite(9614.66, 11176.62, 772.1, 0.16),
-new limite(11176.62, 13381.47, 1022.01, 0.1792),
-new limite(13381.47, 26988.5, 1417.12, 0.2136),
-new limite(26988.5, 42537.58, 4323.58, 0.2352),
-new limite(42537.58, 81211.25, 7980.73, 0.3),
-new limite(81211.25, 108281.67, 19582.83, 0.32),
-new limite(108281.67, 324845.01, 28245.36, 0.34),
-new limite(324845.01, Infinity, 101876.9, 0.35)];
 
 // *********************************************************************************************************************
 // Funciones de utilidad
@@ -86,38 +59,7 @@ const inputString = (mensajePrompt) => {
 }
 
 // ****************************************************************************************************************+
-// Funcion para el calculo de la retencion del Impuesto sobre la renta del salario.
-const calcularISR = () => {
-    // Calculo de la retencion del ISR sobre el salario, basado en los limites establecidos por la ley federal del trabajo mexicana.
-    // Variable para la validacion
-    let mayorCero = false;
-    // Variable para guardar el objeto limite despues de segmentar el salario
-    let limiteAsignado;
-    // Variable para guardar el salario mensual del usuario
-    let salarioMensual;
-    while (!mayorCero) {
-        salarioMensual = inputInt("¿De cuanto es tu salario mensual en pesos (MXN)?");
-        if (salarioMensual >= 0) {
-            // Implementacion de logica de segmentado utilizando un array de objetos limite.
-            limiteAsignado = LIMITES_ISR.find((lim) => { return salarioMensual > lim.limiteInferior && salarioMensual <= lim.limiteSuperior });
-            // Metodo para el calculo de la retencion.
-            limiteAsignado.calcularRetencion(salarioMensual);
-            mayorCero = true;
-        }
-        else {
-            alert("Por favor introduce un valor mayor a 0.");
-        }
-    }
-    let mensaje = `
-    Con un sueldo mensual de $${salarioMensual},
-    tu retención de ISR según la ley federal de trabajo es de:
-    $${limiteAsignado.retencion}, resultando en un ingreso de $${salarioMensual - limiteAsignado.retencion}.
-    `;
-    return mensaje;
-}
-
-// ****************************************************************************************************************+
-// Funcion para el calculo de la vacaciones.
+// Funcion para el calculo de la declaracion anual.
 // Clase para ingresos
 class Ingreso {
     constructor(concepto, mes, monto) {
@@ -199,7 +141,6 @@ const construirDeclaracionAnual = (arrayIngresos, arrayGastos) => {
 
 }
 
-
 const calcularDeclaracionAnual = () => {
     let ingresos = [];
     let gastosDeducibles = [];
@@ -245,54 +186,6 @@ const calcularDeclaracionAnual = () => {
         }
     } while (!salir)
 }
-// *************************************************************************************************************************************************
-// Programa utilizando promtos y alerts 
-
-// let salirMenu = false;
-// let menu = `
-//     Bienvenido a la calculadora fiscal.
-//     Elije una opción para empezar el calculo del concepto.
-//     A - Aguinaldo.
-//     R - Retencion del ISR del salario.
-//     D - Declaración anual.
-//     Introduce 'S' para salir.
-// `;
-
-// Display del menu
-// do {
-//     let opcionSeleccionada = inputString(menu);
-//     switch (opcionSeleccionada) {
-//         case 'A':
-//             alert(`Te corresponde un aguinaldo de $${calcularAguinaldo()}.`);
-//             break;
-//         case 'a':
-//             alert(`Te corresponde un aguinaldo de $${calcularAguinaldo()}.`);
-//             break;
-//         case 'R':
-//             alert(calcularISR());
-//             break;
-//         case 'r':
-//             alert(calcularISR());
-//             break;
-//         case 'D':
-//             calcularDeclaracionAnual();
-//             break;
-//         case 'd':
-//             calcularDeclaracionAnual();
-//             break;
-//         case 'S':
-//             salirMenu = true;
-//             break;
-//         case 's':
-//             salirMenu = true;
-//             break;
-//         default:
-//             alert("Por favor, introduzca una opción válida.")
-//             break;
-//     }
-
-
-// } while (!salirMenu);
 
 // ***********************************************************************************************************
 // Cambio a Event Listeners
@@ -300,6 +193,18 @@ const calcularDeclaracionAnual = () => {
 const valorInput = (input) => {
     return input.value;
 }
+
+//Alertas
+const alertas = document.getElementById("alertas");
+const crearAlerta = () => {
+    alertas.innerHTML = `<div class="alert alert-danger" role="alert">
+    Favor de ingresar información correcta en los campos solicitados.
+    </div>`;
+}
+const limpiarAlerta = () => {
+    alertas.innerHTML = "";
+}
+
 
 // Calculo de Aguinaldo del trabajador
 // ****************************************************************************************************************
@@ -315,18 +220,6 @@ let diasTrabajadosCorrecto = false;
 
 const botonAguinaldo = document.getElementById("botonAguinaldo");
 
-//Alertas
-const alertas = document.getElementById("alertas");
-const crearAlerta = () => {
-    alertas.innerHTML = `<div class="alert alert-danger" role="alert">
-    Favor de ingresar información correcta en los campos solicitados.
-    </div>`;
-}
-const limpiarAlerta = () => {
-    alertas.innerHTML = "";
-}
-
-
 const validacionFormAguinaldo = () => {
     if (anioCumplido) {
         return salarioAguinaldoCorrecto && diasCorrecto;
@@ -335,7 +228,6 @@ const validacionFormAguinaldo = () => {
         return salarioAguinaldoCorrecto && diasCorrecto && diasTrabajadosCorrecto;
     }
 }
-
 // Handlers para cada input
 const validacionSalarioAguinaldo = (e) => {
     if (e.target.value <= 0) {
@@ -415,8 +307,6 @@ radioDiasTrabajadosSi.addEventListener("click", () => {
     anioCumplido = true;
 });
 
-
-
 // Logica del calculo de aguinaldo refactorizada
 const calcularAguinaldo = () => {
     // Si se ha trabajado minimo un año en la empresa
@@ -447,6 +337,4 @@ const aguinaldoHandler = (e) => {
 // Se asigna el event listener
 const formAguinaldo = document.getElementById("calcularAguinaldo");
 formAguinaldo.addEventListener("submit", aguinaldoHandler);
-
-
 
