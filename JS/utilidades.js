206d705864bb5cb3a78e28810f032cd7f7011043
DiegoRugerio97 <diego.rugerioc97@gmail.com>
2022-05-19 18:04:00 -0500

    Se agregan funciones para el cambio de tab

--- JS/utilidades.js
+++ JS/utilidades.js
@@ -21,11 +21,45 @@ const limpiarAlerta = () => {
 }
 
 //funcion utilizando condicional ternaria.
-const habilitarBoton = (isValid,boton) => {
+const habilitarBoton = (isValid, boton) => {
     isValid ? boton.removeAttribute("disabled") : boton.setAttribute("disabled", "");
 }
 
-// Setear el class name 'invalido' para inputs que no pasaron la validacio
-const renderValidacionInput = (input,isValid) =>{
+// Setear el class name 'invalido' para inputs que no pasaron la validacion
+const renderValidacionInput = (input, isValid) => {
     input.className = isValid ? "form-control campoTexto" : "form-control campoTexto invalido";
 }
+
+// Tags a para el cambio de tab
+const tabAguinaldo = document.getElementById("tabAguinaldo");
+const tabRetencion = document.getElementById("tabRetencion");
+const tabAnual = document.getElementById("tabAnual");
+
+// Funcion para resetear forms
+const resetForms = () => {
+    const inputs = document.getElementsByTagName("input");
+    Array.from(inputs).forEach(input => {
+        let containsInvalid = input.className.includes("invalido");
+        if (containsInvalid) {
+            input.className = "form-control campoTexto";
+        }
+        input.value = "";
+    });
+}
+
+const limpiarTab = () => {
+    limpiarAlerta();
+    resetForms();
+}
+
+
+// Funcion para limpiar alerta en caso de cambio de tab
+const cambioTab = (e) => {
+    let isActive = e.target.className.includes("active")
+    !isActive && limpiarTab();
+}
+
+// Se añaden listeners a las tabs
+tabAguinaldo.addEventListener("click", cambioTab);
+tabRetencion.addEventListener("click", cambioTab);
+tabAnual.addEventListener("click", cambioTab);
